d78196ed74f825325c53f4481e765fc7f681d7a4
Alexandra Hendriks <kolozsvari.alexandra25@gmail.com>
2023-03-31 14:24:43 +0200

    added edit event

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -10,107 +10,97 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const EditForm = ({ event, category, users, onClose }) => {
-  const [userEvent, setUserEvent] = useState(event);
+  const [userEvent, setUserEvent] = useState({
+    id: event.id,
+    createdBy: event.createdBy,
+    title: event.title,
+    description: event.description,
+    image: event.image,
+    categoryIds: event.categoryIds,
+    attendedBy: event.attendedBy,
+    location: event.location,
+    startTime: event.startTime,
+    endTime: event.endTime,
+  });
   const [isPending, setIsPending] = useState(false);
   const toast = useToast();
   const history = useNavigate();
   const onSubmit = (data) => {
     console.log(data);
   };
-
-  const handleChange = (e) => {
+  const handleClick = (e) => {
     const { value, checked } = e.target;
     const categoryIds = userEvent.categoryIds;
 
-    const valueExist = categoryIds.includes(Number(value));
-
-    if (checked && !valueExist) {
-      categoryIds.push(Number(value));
-      //console.log(categoryIds);
+    if (checked) {
+      const valueExist = categoryIds.includes(Number(value));
+      if (!valueExist) {
+        categoryIds.push(Number(value));
+        setUserEvent({ ...userEvent, categoryIds: categoryIds });
+      }
     } else {
       const filteredArray = categoryIds.filter((id) => {
-        id == value;
+        return id === Number(value);
       });
-
-      setUserEvent({ ...userEvent, categoryIds: filteredArray });
+      console.log(filteredArray);
+      setUserEvent({ ...userEvent, categoryIds: categoryIds });
     }
+    console.log(userEvent);
   };
-  /* if (!checked && valueExist) {
-      const filterOut = userEvent.categoryIds.filter(
-        (id) => id !== Number(value)
-      );
-      setUserEvent({ ...userEvent, categoryIds: filterOut });
-    } else {
-      categoryIds.push(Number(value));
-    } */
 
-  // console.log(userEvent);
+  //Handlechange function for the input fields
+  const handleChange = (e) => {
+    setUserEvent({ ...userEvent, [e.target.name]: e.target.value });
+    console.log(userEvent);
+  };
+
   return (
     <form onSubmit={() => onSubmit}>
       <FormLabel>
         Title of the event:
-        <Input
-          type="text"
-          value={userEvent.title}
-          onChange={(e) => {
-            setUserEvent({ ...userEvent, title: e.target.value });
-          }}
-        />
+        <Input name="title" value={userEvent.title} onChange={handleChange} />
       </FormLabel>
       <FormLabel>
         Description of the event:
         <Textarea
+          name="description"
           value={userEvent.description}
-          onChange={(e) => {
-            setUserEvent({ ...userEvent, description: e.target.value });
-          }}
+          onChange={handleChange}
         />
       </FormLabel>
       <FormLabel>
         Image of the event in URL form:
         <Input
           type="url"
+          name="image"
           value={userEvent.image}
-          onChange={(e) => {
-            setUserEvent({ ...userEvent, image: e.target.value });
-          }}
+          onChange={handleChange}
         />
       </FormLabel>
       <FormLabel>
         Location of the event:
         <Input
-          type="text"
-          placeholder="Location"
+          name="location"
           value={userEvent.location}
-          onChange={(e) => {
-            setUserEvent({ ...userEvent, location: e.target.value });
-          }}
+          onChange={handleChange}
         />
       </FormLabel>
       <FormLabel>
         Start time:
         <Input
           type="datetime-local"
+          name="startTime"
           value={new Date(userEvent.startTime).toISOString().slice(0, 16)}
-          onChange={(e) => {
-            setUserEvent({
-              ...userEvent,
-              startTime: e.target.value,
-            });
-          }}
+          onChange={handleChange}
         />
       </FormLabel>
       <FormLabel>
         End time:
         <Input
+          name="endTime"
           type="datetime-local"
-          value={new Date(userEvent.endTime).toISOString().slice(0, 16)}
-          onChange={(e) => {
-            setUserEvent({
-              ...userEvent,
-              endTime: e.target.value,
-            });
-          }}
+          defaultValue={new Date(userEvent.endTime).toISOString().slice(0, 16)}
+          onChange={handleChange}
         />
       </FormLabel>
 
@@ -124,7 +114,7 @@ export const EditForm = ({ event, category, users, onClose }) => {
               name={name}
               value={id}
               defaultChecked={userEvent.categoryIds.includes(id)}
-              onChange={handleChange}
+              onChange={(e) => handleClick(e)}
             />
           </FormLabel>
         ))}
